86ffc6698f617d81e88be2f954361a56fac4db3e
daianeluna <daianeluna_admif@hotmail.com>
2023-09-22 19:12:11 -0300

    Resolvendo desafio lista tarefa
    Signed-off-by: daianeluna <daianeluna_admif@hotmail.com>

---
+++ src/list/CarrinhoDeCompras.java
@@ -0,0 +1,63 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    private List<Item> itemList;
+    public CarrinhoDeCompras() {
+
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        itemList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itemParaRemover = new ArrayList<>();
+        for (Item t : itemList) {
+            if (t.getNome().equalsIgnoreCase(nome)) ;
+            {
+                itemParaRemover.add(t);
+            }
+        }
+        itemList.removeAll(itemParaRemover);
+    }
+
+    public double quantidadeValorTotal() {//
+        return itemList.size(); //Para retornar quantidade
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d; //Para retornar o valor
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                double valorItem = item.getPreco() * item.getQuantidade();
+                valorTotal += valorItem; //valorTotal = valorTotal + valorItem;
+            }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public String exibirItens() {
+        return itemList.toString();
+    }
+
+
+    public static void main(String[] args) {
+
+        CarrinhoDeCompras carinhodeCompras = new CarrinhoDeCompras();
+
+        carinhodeCompras.adicionarItem("Item 1", 2,3);
+        carinhodeCompras.adicionarItem("Item 2", 3, 5);
+
+        System.out.println("O número total da lista de itens é " + carinhodeCompras.quantidadeValorTotal());
+        System.out.println("O valor total dos preços da itens é " + carinhodeCompras.calcularValorTotal());
+        System.out.println("O descrição dos itens é " + carinhodeCompras.exibirItens());
+
+    }
+}
