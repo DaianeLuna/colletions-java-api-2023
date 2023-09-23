bd537e4e3b367e19baf90b8567caa631127625d9
daianeluna <daianeluna_admif@hotmail.com>
2023-09-23 10:05:37 -0300

    Resolvendo exercícios do curso de Java - DIO
    Signed-off-by: daianeluna <daianeluna_admif@hotmail.com>

---
+++ src/map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,57 @@
+package map.Ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AgendaEventos {
+
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, evento);
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+            Evento evento = entry.getValue();
+            System.out.println("Data: " + dataEvento + ", Evento: " + evento.getNome() + ", Atração: " + evento.getAtracao());
+        }
+    }
+
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+            if (dataEvento.isEqual(dataAtual) || dataEvento.isAfter(dataAtual)) {
+                proximaData = dataEvento;
+                proximoEvento = entry.getValue();
+                break;
+            }
+        }
+        if (proximoEvento != null) {
+            System.out.println("O próximo evento: " + proximoEvento.getNome() + " acontecerá na data " + proximaData);
+        } else {
+            System.out.println("Não há eventos futuros na agenda.");
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atracao 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.JULY, 15), "Evento 2", "Atracao 2");
+
+        agendaEventos.exibirAgenda();
+    }
+}
